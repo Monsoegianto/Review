8020d561a7d32f068e243b7f6f3cad389b9b580a
kathleenhung5 <43620769+kathleenhung5@users.noreply.github.com>
2019-10-12 17:19:45 -0700

    Update Main.js

--- comps/Main.js
+++ comps/Main.js
@@ -3,6 +3,7 @@ import {View,Text} from 'react-native';
 import styles from '../styles/MainStyles';
 import Top from './Top';
 import Mid from './Mid';
+import Bot from './Bot';
 
 function Main(){
     return(
